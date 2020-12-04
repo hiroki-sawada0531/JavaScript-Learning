a7111560caa047fe658ef82f2d1dd5deed6c93ff
澤田大樹 <hiroki.bigtree@icloud.com>
2020-12-04 23:36:48 +0900

    thisがMemberオブジェクトでない場合に新しくnew演算子でコンストラクターを呼び出す。

---
+++ JavaScript_learn/simple3.js
@@ -0,0 +1,10 @@
+var Member = function (firstName, lastName) {
+  if (!(this instanceof Member)) {
+    return new Member(firstName, lastName);
+  }
+  this.firstName = firstName;
+  this.lastName = lastName;
+};
+
+var m = Member("権兵衛", "佐藤");
+console.log(m);
