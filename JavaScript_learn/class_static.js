fd7d7446dbc62102b2df93987755a45ae4470a5e
澤田大樹 <hiroki.bigtree@icloud.com>
2020-12-08 23:54:05 +0900

    静的メソッドを定義する

---
+++ JavaScript_learn/class_static.js
@@ -0,0 +1,41 @@
+class Area {
+    static getTriangle(base, height) {
+        return base * height / 2;
+    }
+}
+
+console.log(Area.getTriangle(10,5));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
