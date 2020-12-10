287bbcc1a752b105bd1f384196cd7ccdd47e0df1
澤田大樹 <hiroki.bigtree@icloud.com>
2020-12-10 23:45:49 +0900

    モジュールのインポート

--- JavaScript_learn/lib/Util.js
+++ JavaScript_learn/lib/Util.js
@@ -1,5 +1,18 @@
 const AUTHOR = 'YAMADA Yoshihiro';
 
-export class Member {}
+export class Member {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
-export class Area {}
+    getName() {
+        return this.lastName + this.firstName;
+    }
+}
+
+export class Area {
+    static getTriangle(base, height) {
+        return base * height / 2;
+    }
+}
