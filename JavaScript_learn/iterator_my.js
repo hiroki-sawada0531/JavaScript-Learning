54b0dd667f2a6e07019f8e95154e75643ef6723c
澤田大樹 <hiroki.bigtree@icloud.com>
2020-12-11 22:35:23 +0900

    イテレーターの利用方法

---
+++ JavaScript_learn/iterator_my.js
@@ -0,0 +1,22 @@
+class MyIteretor {
+    constructor(data) {
+        this.data = data;
+    }
+
+[Symbol.iterator](){
+    let current = 0;
+    let that = this;
+    return {
+        next() {
+            return current < that.data.length ?
+            {value: that.data[current++], done: false} :
+            {done: true};
+        }
+    };
+}
+}
+
+let itr = new MyIteretor(['one', 'two', 'three']);
+for(let value of itr) {
+    console.log(value);
+}
