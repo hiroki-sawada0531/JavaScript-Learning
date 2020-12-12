f89206b00fcbb083895e8a430f70f3dadefa3bdb
澤田大樹 <hiroki.bigtree@icloud.com>
2020-12-12 20:32:09 +0900

    修正

--- JavaScript_learn/element_name.js
+++ JavaScript_learn/element_name.js
@@ -1,3 +1,3 @@
 var current = new Date();
-var num = document.getElementsByName('time');
-nam[0].value = current.toLocaleTimeString();
+var nam = document.getElementsByName('time');
+num[0].value = current.toLocaleTimeString();
