465f48819e08125513ffdd42eed9f44df8a42365
澤田大樹 <hiroki.bigtree@icloud.com>
2020-12-19 22:15:43 +0900

    ファイルの読み込みに失敗した時のエラー処理の実装

--- JavaScript_learn/file_reader.js
+++ JavaScript_learn/file_reader.js
@@ -5,6 +5,10 @@ window.addEventListener('DOMContentLoaded', function() {
         reader.addEventListener('load', function() {
             document.getElementById("result").textContent = reader.result;
         }, true);
+        reader.addEventListener('error', function() {
+            console.log(reader.error.message);
+        }, true);
         reader.readAsText(input, 'UTF-8');
+        reader.abort();
     }, true);
 });
