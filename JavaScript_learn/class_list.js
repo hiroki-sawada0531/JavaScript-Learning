10f52e997042b5e52ea2f1d194010260bd1f20ec
澤田大樹 <hiroki.bigtree@icloud.com>
2020-12-26 00:05:49 +0900

    classListプロパティで書き換えtoggleメソッド

---
+++ JavaScript_learn/class_list.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elem = document.getElementById('elem');
+    elem.addEventListener('click', function() {
+        this.classList.toggle('highlight');
+    }, false);
+}, false);
