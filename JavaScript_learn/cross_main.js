28b036a47de87017e2840efc998852341866eeb7
澤田大樹 <hiroki.bigtree@icloud.com>
2021-01-16 22:31:05 +0900

    クロスドキュメントメッセージングによるクロスオリジン通信

---
+++ JavaScript_learn/cross_main.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var target = 'http://www.wings.msn.to';
+
+    document.getElementById('btn').addEventListener('click', function() {
+        document.getElementById('frame').contentWindow.postMessage(
+            document.getElementById('message').nodeValue, target
+        );
+    }, false);
+    window.addEventListener('message', function(e) {
+        if (e.origin !== target) { return; }
+        console.log(e.data);
+        }, false);
+    }, false);
